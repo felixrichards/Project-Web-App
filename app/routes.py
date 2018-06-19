fe9661cbfe1f7334b8845db80e11f3035099a20d
Felix Richards <felixarichards@gmail.com>
2018-06-19 21:13:52 +0100

    Clean comments

--- app/routes.py
+++ app/routes.py
@@ -25,31 +25,3 @@ def index():
     return render_template('index.html', title='Home',user=user)
     
     
-# @app.route('/annotate')
-# def annotate():
-    # galaxy=get_random_galaxy()
-    # return redirect(url_for('annotate_by_id',g_id=galaxy.g_id))
-
-# @app.route('/annotate/name/<name>')
-# def annotate_by_name(name):
-    # galaxy=Galaxy.query.filter_by(name=name).one_or_none()
-    # if (galaxy==None):
-        # return annotate()
-    # return annotate_by_id(galaxy.g_id)
-    
-    
-# @app.route('/annotate/id/<int:g_id>',methods=['GET','POST'])
-# def annotate_by_id(g_id):
-    # galaxy=Galaxy.query.get(g_id)
-    
-    # if request.method=='POST':
-        # shapes=request.get_json()
-        # a = Annotation(galaxy)
-        # print(shapes[0])
-    
-    
-    # dim={'width':800,'height':700,'image_width':500,'image_height':500}
-    # return render_template('annotate.html',title='Annotate',file=galaxy,dim=dim)
-    
-
-    
