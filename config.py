d5bc90ac1a9f15100e605be533a08afc1dacd766
Felix Richards <felixarichards@gmail.com>
2018-10-01 15:47:09 +0100

    Prepare for DB migration to Postgres

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY="mflskmvoi4j-8hp;aoirja[309eia['wodjasdsfcnvoasmdca3"
+    CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
