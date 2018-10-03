4211640ba37e9d58fda744af93786636f8b8702e
Felix Richards <felixarichards@gmail.com>
2018-10-03 13:56:44 +0100

    Blueprint refactor

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,25 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-csrf = CSRFProtect(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+csrf = CSRFProtect()
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app import routes, models
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    csrf.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+from app import models
