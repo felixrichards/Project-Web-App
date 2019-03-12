d0c6fc821f3757a85084bb9ceb0578360ae6722e
Felix Richards <felixarichards@gmail.com>
2019-03-12 19:22:31 +0000

    setup files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,2 @@
 flask db upgrade
-flask shell <<@@
-g=Galaxy(name='dummy')
-db.session.add(g)
-ua=[User(username='paduc'),User(username='felix'),User(username='adeline')]
-for u in ua:
-    ua.set_access(3)
-    db.session.add(u)
-db.session.commit()
-@@
+python setupdb.py
--- setupdb.py
+++ setupdb.py
@@ -1,10 +1,13 @@
-from app import db
+from app import db, create_app()
 from app.models import Galaxy, User
 
-g=Galaxy(name='dummy')
-db.session.add(g)
-ua=[User(username='paduc'),User(username='felix'),User(username='adeline')]
-for u in ua:
-    ua.set_access(3)
-    db.session.add(u)
-db.session.commit()
+app = create_app()
+with app.app_context():
+    g=Galaxy(name='dummy')
+    db.session.add(g)
+    ua=[User(username='paduc'),User(username='felix'),User(username='adeline')]
+    for u in ua:
+        u.set_access(3)
+        db.session.add(u)
+    db.session.commit()
+    print("added")
