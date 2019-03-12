484f91cf8e2b3df40223b87c074d1abc6dcd6eee
Felix Richards <felixarichards@gmail.com>
2019-03-12 18:09:00 +0000

    setup files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,2 @@
-pip install flask
-pip install sqlalchemy
-pip install flask_sqlalchemy
-pip install flask_migrate
-pip install flask_wtf
-export FLASK_APP=GC.py
-flask run
+flask db upgrade
+python setupdb.py
--- setupdb.py
+++ setupdb.py
@@ -1,12 +1,10 @@
-# from app import db
-# from app.models import Galaxy
+from app import db
+from app.models import Galaxy, User
 
-# g1=Galaxy(name="NGC-1300",survey="NGC")
-# g2=Galaxy(name="NGC-3923",survey="NGC")
-# g3=Galaxy(name="NGC-4414",survey="NGC")
-# g4=Galaxy(name="NGC-5457",survey="NGC")
-# db.session.add(g1)
-# db.session.add(g2)
-# db.session.add(g3)
-# db.session.add(g4)
-# db.session.commit()
+g=Galaxy(name='dummy')
+db.session.add(g1)
+ua=[User(username='paduc'),User(username='felix'),User(username='adeline')]
+for u in ua:
+    ua.set_access(3)
+    db.session.add(u)
+db.session.commit()
