0d609ac811021a9c721e3dd96e71b4a9872c0c03
Felix Richards <felixarichards@gmail.com>
2019-03-12 18:09:36 +0000

    setup files

--- setupdb.py
+++ setupdb.py
@@ -2,7 +2,7 @@ from app import db
 from app.models import Galaxy, User
 
 g=Galaxy(name='dummy')
-db.session.add(g1)
+db.session.add(g)
 ua=[User(username='paduc'),User(username='felix'),User(username='adeline')]
 for u in ua:
     ua.set_access(3)
