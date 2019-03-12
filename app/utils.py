15ba467a4911db5c82f6ce5b903eed068668bcc3
Felix Richards <felixarichards@gmail.com>
2019-03-12 19:28:04 +0000

    corrected random generation

--- app/utils.py
+++ app/utils.py
@@ -5,5 +5,5 @@ import random
 def get_random_galaxy(survey=None):
     if survey is None:
         start = Galaxy.query.first().g_id
-        rand = random.randrange(start, start+Galaxy.query.count()+1)
+        rand = random.randrange(start, start+Galaxy.query.count())
     return Galaxy.query.get(rand)
