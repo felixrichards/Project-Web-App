a499b0d89d5e8e4d2d29cd6fc3fb8e8f9557788a
Felix Richards <felixarichards@gmail.com>
2019-03-12 18:47:52 +0000

    setup files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 flask db upgrade
-python setupdb.py
+flask shell <<@@
+g=Galaxy(name='dummy')
+db.session.add(g)
+ua=[User(username='paduc'),User(username='felix'),User(username='adeline')]
+for u in ua:
+    ua.set_access(3)
+    db.session.add(u)
+db.session.commit()
+@@
